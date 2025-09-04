0e0e1762a46514964af8ec52e3a9e3a7d488980e
Qetmeer <qitmeershehzad@gmail.com>
2025-09-04 21:04:30 +0500

    updated code

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -2,12 +2,15 @@ import express from "express";
 import { addProduct, removeProduct, listProduct, singleProduct } from "../controller/productController.js";
 import { upload } from "../helpers/upload.js";
 import multer from "multer";
+import adminauth from "../middleware/AdminAuth.js";
 const router = express.Router();
 
 // ✅ single field name "images"
 
 router.post(
   "/add",
+  adminauth
+  ,
   (req, res, next) => {
     upload(req, res, function(err) {
       if (err instanceof multer.MulterError) {
@@ -28,7 +31,7 @@ router.post(
 
 
 
-router.delete("/remove/:id", removeProduct);
+router.delete("/remove/:id",adminauth, removeProduct);
 router.get("/single/:id", singleProduct);
 router.get("/list", listProduct);
 
