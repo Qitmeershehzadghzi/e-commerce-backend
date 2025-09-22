26c950a3449948860136c802d1768b2c036ea045
Qitmeershehzadghzi <qitmeershehzad@gmail.com>
2025-09-22 18:55:19 +0500

    updated code

--- controller/orderController.js
+++ controller/orderController.js
@@ -4,8 +4,8 @@ import productModel from '../models/productModel.js'
 
 
 // placing order using cod method
- export const placeOreder =async (req,res)=>{
-    try {
+export const placeOreder = async (req, res) => {
+  try {
     const { userId, items, amount, address } = req.body;
 
     // Har product ka detail nikalna (name, image, price)
@@ -52,20 +52,20 @@ import productModel from '../models/productModel.js'
   }
 }
 // placing order using Stripe  method
-export const placeOrederStripe =async (req,res)=>{
-    try {
-        
-    } catch (error) {
-        
-    }
+export const placeOrederStripe = async (req, res) => {
+  try {
+
+  } catch (error) {
+
+  }
 }
 // placing order using Razorpay method
-export const placeOrederRazorpay =async (req,res)=>{
-    try {
-        
-    } catch (error) {
-        
-    }
+export const placeOrederRazorpay = async (req, res) => {
+  try {
+
+  } catch (error) {
+
+  }
 }
 
 
@@ -85,27 +85,30 @@ export const allOrders = async (req, res) => {
     });
   }
 };
-
-
 //  user order data from frontend
-export const userOrders =async (req,res)=>{
-    try {
-        const {userId} =req.body
-        const orders =await orderModel.find({userId})
-        res.json({success:true,orders})
-    } catch (error) {
-        console.log(error);
-        
-                   res.json({success:true,message:error.message})
-    }
+export const userOrders = async (req, res) => {
+  try {
+    const { userId } = req.body
+    const orders = await orderModel.find({ userId })
+    res.json({ success: true, orders })
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message })
+
+  }
 }
 
 
 // update order status from admin panel
-export const updateStatus =async (req,res)=>{
-    try {
-        
-    } catch (error) {
-        
-    }
+export const updateStatus = async (req, res) => {
+  try {
+    const { orderId, status } = req.body
+    await orderModel.findByIdAndUpdate(orderId, { status })
+    res.json({ success: true, message: 'Status updated' })
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message })
+
+
+  }
 }
